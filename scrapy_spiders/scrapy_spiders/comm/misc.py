957f8e4941e8b3d869ed58ebe29076cf433c3007
sheri528 <gongyankun@live.cn>
2018-04-25 11:31:33 +0800

    add decorator for limiting calls

--- scrapy_spiders/scrapy_spiders/comm/misc.py
+++ scrapy_spiders/scrapy_spiders/comm/misc.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import time
 from functools import wraps
+
 
 def check_response(func):
     @wraps(func)
@@ -26,3 +28,33 @@ def check_response(func):
 
     return is_normal
 
+
+def timethis(func):
+    '''
+        Decorator that limits the execution period not less than 10s.
+    '''
+
+    _last_time = [0]
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        if _last_time[0] == 0 or start - _last_time[0] > 10:
+            result = func(*args, **kwargs)
+            _last_time[0] = start
+            print("success. call {0}, time is {1}".format(func.__name__, _last_time[0]))
+            return result
+        else:
+            print("limit call 10s, last call time is {0}".format(_last_time[0]))
+
+    return wrapper
+
+# @timethis
+# def func(n):
+#     for i in range(n):
+#         pass
+#
+# func(400)
+# func(2)
+# time.sleep(10)
+# func(30)
